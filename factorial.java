39f36ba357cb591c5111451ac913f6a93fee8dbb
crackernabin <crackerboy9797@gmail.com>
2026-02-23 01:15:04 +0530

    https://github.com/crackernabin/JAVA-Sheet1-method-recursion-

---
+++ factorial.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class factorial {
+    static long fact(int n) {
+        if(n==0 || n==1){
+            return 1;
+        } else {
+            return n*fact(n-1);
+        }
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the number:");
+        int n=sc.nextInt();
+        if(n>=0 && n<=20){
+     System.out.println("factorial number is"+fact(n));
+        }else{
+            System.out.println("number is invalid");
+        }
+    }
+}
+
